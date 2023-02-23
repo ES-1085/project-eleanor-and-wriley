a74d4e3833bcc4fbbb6d19b346f50edcb3ced62a
eleanorjgnam <egnam23@coa.edu>
2023-02-23 10:47:49 -0500

    Update shiny app, launch shiny app

--- towercount/app.R
+++ towercount/app.R
@@ -9,30 +9,56 @@
 library(shiny)
 library(tidyverse)
 
-
-
+#Import Data
 tower <- read.csv("Data/towerclean.csv")
-yearlist <- as.list(unique(towerclean$year))
-# Define UI for application that draws a histogram
+yearlist <- as.list(unique(tower$year))
+specieslist <- as.list(unique(tower$species))
+
+#########################################################
+###################### U I ##############################
+#########################################################
+
 ui <- fluidPage(
   titlePanel("Tower Count Data"),
-  
   sidebarLayout(
-    sidebarPanel("Year"),
-
-column(3,
-         selectInput("select", h3("Select box"), 
-                     choices = yearlist, selected = 1)),
-mainPanel(
-  textOutput("selected_year"))
-)
+    sidebarPanel(
+        selectInput("yearIn", "Year:", choices = yearlist, multiple = TRUE, selected = 2000),
+        checkboxGroupInput("speciesIn", "Species", choices = specieslist, selected  = c("herg","gbbg","coei_ad","blgu")),
+        selectInput("plottype","Plot Type", choices = c("barplot","boxplot"))
+    ),
+    mainPanel(
+      plotOutput("plot"))
+  )
 )
 
 # Define server logic to plot various variables against mpg ----
 server <- function(input, output) {
   
-  output$selected_year <- renderText({ 
-    "You have selected this"
+  output$plot <- renderPlot({
+     if (input$plottype == "barplot") {
+        tower %>% 
+        group_by(species, year) %>% 
+        summarize(max = max(count)) %>% 
+        filter(year %in% input$yearIn) %>% 
+        filter(species %in% input$speciesIn) %>% 
+        ggplot(aes(x = species, y = max, fill = species))+
+        geom_col()+
+        facet_wrap(~year)+
+        labs(title= "High Counts", subtitle = "Great Duck Tower Data")+
+        scale_fill_viridis_d()+
+        theme_bw()
+    } else if (input$plottype == "boxplot") {
+        tower %>% 
+        group_by(species, year) %>% 
+        filter(year %in% input$yearIn) %>% 
+        filter(species %in% input$speciesIn) %>% 
+        ggplot(aes(x = species, y = count, fill = species))+
+        geom_boxplot()+
+        facet_wrap(~year)+
+        labs(title= "All Counts", subtitle = "Great Duck Tower Data")+
+        scale_fill_viridis_d()+
+        theme_bw()
+    }
   })
   
 }
